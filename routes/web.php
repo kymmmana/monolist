d0143e4bd6a56631cead8b830b4913f745fcfa22
EC2 Default User <ec2-user@ip-172-31-11-75.us-east-2.compute.internal>
2018-06-15 05:55:06 +0000

    have ranking

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
